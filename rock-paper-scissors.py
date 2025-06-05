48e9e728f9ecd44728e048424a26a627b319479d
Roman Khadraliev <Romch33tos@yandex.ru>
2025-06-05 21:13:16 +0700

    Обновление для менеджера геометрии

--- rock-paper-scissors.py
+++ rock-paper-scissors.py
@@ -51,7 +51,9 @@ def show_instructions():
     messagebox.showinfo(
         title="Справка", 
         message="Инструкция", 
-        detail="1. Нажми на любой жест, чтобы\nначать игру.\n2. Выбранный жест отобразится\nв левой части окна."
+        detail="1. Нажми на любой жест, чтобы
+начать игру.\n2. Выбранный жест отобразится
+в левой части окна."
     )
 
 def show_rules():
@@ -73,7 +75,7 @@ def resize_image(path, size):
 
 root = tk.Tk()
 root.title("Камень-ножницы-бумага!")
-root.geometry("560x360")
+root.geometry("480x350")
 root.resizable(width=False, height=False)
 
 if not os.path.exists("Жесты"):
@@ -86,53 +88,61 @@ help_menu.add_command(label="Инструкция", command=show_instructions)
 help_menu.add_command(label="Правила игры", command=show_rules)
 root.config(menu=menu_bar)
 
-player_choice_label = tk.Label(root)
-player_choice_label.grid(row=0, column=0, sticky=tk.NW, padx=10, pady=20)
-computer_choice_label = tk.Label(root)
-computer_choice_label.grid(row=0, column=0, sticky=tk.NW, padx=380, pady=20)
+gestures_frame = tk.Frame(root, height=150)
+gestures_frame.grid(row=0, column=0, sticky="ew", padx=10, pady=20)
+gestures_frame.grid_propagate(False)
+
+player_choice_label = tk.Label(gestures_frame)
+player_choice_label.place(x=20, y=25)
+
+computer_choice_label = tk.Label(gestures_frame)
+computer_choice_label.place(x=320, y=25)
 
 status_label = tk.Label(root, text="", font=('Arial', 14))
-status_label.grid(row=0, column=0, padx=10, pady=180, sticky=tk.NW)
+status_label.grid(row=1, column=0, padx=10, pady=5, sticky="w")
 
 prompt_label = tk.Label(root, text="Выбери свой жест:", font=('Arial', 14))
-prompt_label.grid(row=0, column=0, padx=10, pady=220, sticky=tk.NW)
+prompt_label.grid(row=2, column=0, padx=10, pady=5, sticky="w")
+
+buttons_frame = tk.Frame(root)
+buttons_frame.grid(row=3, column=0, sticky="ew", pady=10)
 
 rock_button = tk.Button(
-    root, 
+    buttons_frame, 
     width=10, 
     height=2, 
     text="Камень", 
     font=('Arial', 14), 
     command=lambda: prepare_gesture(show_rock)
 )
-rock_button.grid(row=0, column=0, sticky=tk.NW, padx=20, pady=270)
+rock_button.pack(side="left", padx=20, expand=True)
 
 scissors_button = tk.Button(
-    root, 
+    buttons_frame, 
     width=10, 
     height=2, 
     text="Ножницы", 
     font=('Arial', 14), 
     command=lambda: prepare_gesture(show_scissors)
 )
-scissors_button.grid(row=0, column=0, sticky=tk.NW, padx=220, pady=270)
+scissors_button.pack(side="left", padx=20, expand=True)
 
 paper_button = tk.Button(
-    root, 
+    buttons_frame, 
     width=10, 
     height=2, 
     text="Бумага", 
     font=('Arial', 14), 
     command=lambda: prepare_gesture(show_paper)
 )
-paper_button.grid(row=0, column=0, sticky=tk.NW, padx=420, pady=270)
+paper_button.pack(side="left", padx=20, expand=True)
 
-rock_img_player = resize_image("Жесты/rock.png", (140, 100))
-scissors_img_player = resize_image("Жесты/scissors.png", (150, 150))
-paper_img_player = resize_image("Жесты/paper.png", (150, 150))
-rock_img_computer = resize_image("Жесты/rock2.png", (140, 100))
-scissors_img_computer = resize_image("Жесты/scissors2.png", (150, 150))
-paper_img_computer = resize_image("Жесты/paper2.png", (150, 150))
+rock_img_player = resize_image("Жесты/rock.png", (120, 100))
+scissors_img_player = resize_image("Жесты/scissors.png", (120, 120))
+paper_img_player = resize_image("Жесты/paper.png", (120, 120))
+rock_img_computer = resize_image("Жесты/rock2.png", (120, 100))
+scissors_img_computer = resize_image("Жесты/scissors2.png", (120, 120))
+paper_img_computer = resize_image("Жесты/paper2.png", (120, 120))
 
 root.protocol("WM_DELETE_WINDOW", cleanup)
 root.mainloop()

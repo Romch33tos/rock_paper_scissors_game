2c0ae8863993f067c40c0a1e01b65d71ea74a457
Roman Khadraliev <Romch33tos@yandex.ru>
2025-06-05 20:25:02 +0700

    Изменения для UI
    Была убрана статистическая информация

--- rock-paper-scissors.py
+++ rock-paper-scissors.py
@@ -2,21 +2,14 @@ import tkinter as tk
 from tkinter import *
 from tkinter import messagebox as mb
 import random
-
-list1 = [ 'Камень', 'Ножницы', 'Бумага']
-
-w = 0
 l = 0
 d = 0
-
+w = 0
+list1 = [ 'Камень', 'Ножницы', 'Бумага']
 root = Tk()
-
 txt = StringVar()
 txt2 = StringVar()
 txt3 = StringVar()
-
-#Текстовый виджет
-
 def rock():
     answer = random.choice(list1)
     text.configure(state = NORMAL)
@@ -44,8 +37,7 @@ def rock():
         txt2.set(str(l))      
         text.insert(END, "\nМой выбор: " + str(answer))
         text.insert(END, "\nЯ победил!")
-        text.configure(state = DISABLED)
-    
+        text.configure(state = DISABLED)    
 def scissors():
     answer = random.choice(list1)
     text.configure(state = NORMAL)
@@ -73,8 +65,7 @@ def scissors():
         txt.set(str(w))        
         text.insert(END, "\nМой выбор: " + str(answer))
         text.insert(END, "\nЯ проиграл!")
-        text.configure(state = DISABLED)
-    
+        text.configure(state = DISABLED)    
 def paper():
     answer = random.choice(list1)
     text.configure(state = NORMAL)
@@ -102,36 +93,13 @@ def paper():
         txt3.set(str(d))        
         text.insert(END, "\nМой выбор: " + str(answer))
         text.insert(END, "\nНичья!")
-        text.configure(state = DISABLED) 
-    
-#Текстовые надписи
+        text.configure(state = DISABLED)
 
 l1 = Label(root, text = "Камень, ножницы, бумага!")
 l1.grid(row = 0, column = 0, padx = 165, pady = 7, sticky = NW)
 
-l2 = Label(root, text="Количество побед: ") 
-l2.grid(row = 2, column = 0, sticky = NW, padx = 60, pady = 7)
-
-l3 = Label(root, text="Количество поражений:")
-l3.grid(row = 3, column = 0, sticky = NW, padx = 60, pady = 7)
-
-l4 = Label(root, text = "Количество ничьих: ")
-l4.grid(row = 4, column = 0, padx = 60,  pady = 7, sticky = NW)
-
 l5 = Label(root, text = "Выбери свой жест: ")
 l5.grid(row = 5, column = 0, padx = 60,  pady = 7, sticky = NW)
-
-win = Entry(root, width = 13, justify = CENTER, textvariable=txt)
-win.configure(disabledbackground="white", disabledforeground="black", state = DISABLED)
-win.grid(row = 2, column = 0, sticky = NW, padx = 415, pady = 7)
-
-lose = Entry(root, width = 13, justify = CENTER, textvariable=txt2)
-lose.configure(disabledbackground="white", disabledforeground="black", state = DISABLED)
-lose.grid(row = 3, column = 0, sticky = NW, padx = 415, pady = 7)
-
-draw = Entry(root, width = 13, justify = CENTER, textvariable=txt3)
-draw.configure(disabledbackground="white", disabledforeground="black", state = DISABLED)
-draw.grid(row = 4, column = 0, sticky = NW, padx = 415, pady = 7)
 
 text = Text(width = 36, height = 5)
 text.grid(row = 1, column = 0, padx = 60,pady = 5, sticky = NW)
